4e3115aa66add587b309cc7cc8f591f32b6c77a0
mcuozzo19 <madelinecuozzo3@gmail.com>
2020-06-26 15:55:43 +0000

    import datastore libraries to servlet and update comment

--- website/src/main/java/com/google/sps/servlets/MockDataServlet.java
+++ website/src/main/java/com/google/sps/servlets/MockDataServlet.java
@@ -14,6 +14,16 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.Date;
