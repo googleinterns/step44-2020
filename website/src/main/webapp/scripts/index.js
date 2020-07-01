5ae4edfd8f85c377b1ce1155fa8dcd4f8136a5c8
PaulLockett <plockett.pl@gmail.com>
2020-07-01 16:33:30 +0000

    auto formated Js with clang-format

--- website/src/main/webapp/scripts/index.js
+++ website/src/main/webapp/scripts/index.js
@@ -1,19 +1,19 @@
 async function getQueue(query) {
   const restaurants = await fetch('/searchRequest?query=' + query)
-    .then(response => response.json())
-    .then((restaurantsData) => {
-      return restaurantsData["results"];
-    });
+                          .then(response => response.json())
+                          .then((restaurantsData) => {
+                            return restaurantsData['results'];
+                          });
 
   console.log(restaurants);
 }
 
 async function getVolumeData() {
   fetch('/MockData')
-    .then(response => response.json())
-    .then((restaurantVolumeData) => {
-      restaurantVolumeData.forEach((restaurant) => {
-        console.log(restaurant);
-      })
-    });
+      .then(response => response.json())
+      .then(
+          (restaurantVolumeData) => {
+              restaurantVolumeData.forEach((restaurant) => {
+                console.log(restaurant);
+              })});
 }
