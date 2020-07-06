7b620a65a554f78ba320b26ca23e4e8c92eb21f2
Yojairo Morales <yojairom@usc.edu>
2020-07-06 21:07:49 +0000

    Implement restaurant and mock data retrieval in a function to later display onto website.

--- website/src/main/webapp/scripts/index.js
+++ website/src/main/webapp/scripts/index.js
@@ -1,19 +1,36 @@
+//TODO: Encapsulate the function below into smaller functions.
 async function getQueue(query) {
   const restaurants = await fetch('/searchRequest?query=' + query)
-                          .then(response => response.json())
-                          .then((restaurantsData) => {
-                            return restaurantsData['results'];
-                          });
+    .then(response => response.json())
+    .then((restaurantsData) => {
+      return restaurantsData['results'];
+    });
+
+  const volumeData = await fetch('/MockData')
+    .then(response => response.json())
+    .then(
+      (restaurantVolumeData) => {
+        return restaurantVolumeData;
+      });
 
   console.log(restaurants);
+  console.log(volumeData);
+
+}
+
+async function getRestaurants(query) {
+  fetch('/searchRequest?query=' + query)
+    .then(response => response.json())
+    .then((restaurantsData) => {
+      return restaurantsData['results'];
+    });
 }
 
 async function getVolumeData() {
   fetch('/MockData')
-      .then(response => response.json())
-      .then(
-          (restaurantVolumeData) => {
-              restaurantVolumeData.forEach((restaurant) => {
-                console.log(restaurant);
-              })});
+    .then(response => response.json())
+    .then(
+      (restaurantVolumeData) => {
+        return restaurantVolumeData;
+      });
 }
