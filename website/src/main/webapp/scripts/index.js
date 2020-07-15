8d257c7afe87faa1d90bc983d9d43878b388a162
Yojairo Morales <yojairom@usc.edu>
2020-07-15 15:36:06 +0000

    Implement queue length bar and stars.

--- website/src/main/webapp/scripts/index.js
+++ website/src/main/webapp/scripts/index.js
@@ -15,18 +15,30 @@ async function getQueue(query) {
 
   console.log(restaurants);
   console.log(volumeData);
+  volumeDataIndex = 1;
 
   const results = document.getElementById('results');
 
   restaurants.forEach((restaurant) => {
-    console.log(restaurant['name']);
-    console.log(restaurant['formattedAddress']);
-    console.log(restaurant['rating']);
+    stars = "";
+
+    for (i = 0; i < parseInt(restaurant['rating']); i++) {
+      stars += '<i class="fa fa-star" aria-hidden="true"></i>';
+    }
+
+    if (parseFloat(restaurant['rating']) - parseInt(restaurant['rating']) > .5) {
+      stars += '<i class="fa fa-star-half-o" aria-hidden="true"></i>';
+    }
 
     results.innerHTML += '<div class="m-1 card"><div class="card-body"><h5 class="card-title">'
       + restaurant['name'] + '</h5><h6 class="card-subtitle mb-2 text-muted">'
       + restaurant['formattedAddress'] + '</h6><p class="card-text">'
-      + restaurant['rating'] + '</p><a href="#" class="card-link"><i class="fa fa-angle-down"></i></a></div></div>';
+      + stars + '</p><a href="#" class="card-link"><i class="fa fa-angle-down"></i></a></div>'
+      + '<div class="progress"><div class="progress-bar" role="progressbar" style="width: '
+      + volumeDataIndex * 5 + '% " aria-valuenow=" ' + volumeDataIndex * 5 + '" aria-valuemin="0"aria-valuemax="100"></div></div>'
+      + '</div>';
+
+    volumeDataIndex++;
   });
 
 }
