59da31ee82dc01af564a17d9978ae5de4b633ddc
Yojairo Morales <yojairom@usc.edu>
2020-07-16 19:29:50 +0000

    Replace progress bar with persons to indicate length.

--- website/src/main/webapp/scripts/index.js
+++ website/src/main/webapp/scripts/index.js
@@ -18,6 +18,7 @@ async function getQueue(query) {
   volumeDataIndex = 1;
 
   const results = document.getElementById('results');
+  results.innerHTML = "";
 
   restaurants.forEach((restaurant) => {
     stars = "";
@@ -30,13 +31,17 @@ async function getQueue(query) {
       stars += '<i class="fa fa-star-half-o" aria-hidden="true"></i>';
     }
 
+    color = getColor(volumeDataIndex);
+
+    lineLength = buildLine(volumeDataIndex, color);
+
+    console.log(lineLength)
+
     results.innerHTML += '<div class="m-1 card"><div class="card-body"><h5 class="card-title">'
       + restaurant['name'] + '</h5><h6 class="card-subtitle mb-2 text-muted">'
       + restaurant['formattedAddress'] + '</h6><p class="card-text">'
-      + stars + '</p><a href="#" class="card-link"><i class="fa fa-angle-down"></i></a></div>'
-      + '<div class="progress"><div class="progress-bar" role="progressbar" style="width: '
-      + volumeDataIndex * 5 + '% " aria-valuenow=" ' + volumeDataIndex * 5 + '" aria-valuemin="0"aria-valuemax="100"></div></div>'
-      + '</div>';
+      + stars + '</p><a href="#" class="card-link">' + lineLength
+      + '<i class="fa fa-angle-down"></i></a></div></div>';
 
     volumeDataIndex++;
   });
@@ -58,4 +63,27 @@ async function getVolumeData() {
       (restaurantVolumeData) => {
         return restaurantVolumeData;
       });
+}
+
+function getColor(volumeData) {
+  if (volumeData < 6) {
+    ;
+    return '#008000';
+  } else if (volumeData < 13) {
+    return '#CCCC00';
+  } else {
+    return '#FF0000';
+  }
+}
+
+function buildLine(volumeData, color) {
+  line = "<div>";
+
+  for (i = 0; i < volumeData; i++) {
+    line += '<i class="m-1 fa fa-user" style="color: ' + color + '" aria-hidden="true"></i>';
+  }
+
+  line += "</div>";
+
+  return line;
 }
